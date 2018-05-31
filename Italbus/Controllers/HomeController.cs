1dad202cd72466b4fa7fe2227627a493f654546c
L666F <fiorani.luca1998@gmail.com>
2018-05-31 11:38:56 +0200

    Added Login Feature

--- Italbus/Controllers/HomeController.cs
+++ Italbus/Controllers/HomeController.cs
@@ -5,14 +5,65 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Italbus.Models;
+using Microsoft.AspNetCore.Identity;
+using Italbus.Models.ViewModels;
 
 namespace Italbus.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly SignInManager<ApplicationUser> signInManager;
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public HomeController(SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager)
+        {
+            this.signInManager = signInManager;
+            this.userManager = userManager;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Login()
+        {
+            if (signInManager.IsSignedIn(User))
+                return RedirectToAction("Index", "Admin");
+            else
+                return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginVM vm)
+        {
+            vm.Message = null;
+            if (ModelState.IsValid)
+            {
+                var user = await userManager.FindByNameAsync(vm.Username);
+                if (user != null)
+                {
+                    var result = await signInManager.PasswordSignInAsync(user, vm.Password,false,false);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index", "Admin");
+                    }
+                    else
+                    {
+                        vm.Message = "Wrong Username or Password";
+                        return View(vm);
+                    }
+                }
+                else
+                {
+                    vm.Message = "User not found.";
+                    return View(vm);
+                }
+            }
+            else
+            {
+                return View(vm);
+            }
         }
 
         public IActionResult Error()
